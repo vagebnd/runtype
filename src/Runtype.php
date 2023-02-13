138637e8ae419678bd8236abb281f6bc7cd8ae4f
Gianluca Riggio <luca@maximumawesome.nl>
2023-02-13 23:15:27 +0100

    added runtype implementation

---
+++ src/Runtype.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Vagebond\Runtype;
+
+use Symfony\Component\Finder\Finder;
+use Vagebond\Runtype\Actions\ConvertEntities;
+use Vagebond\Runtype\Actions\PersistTypescriptTypes;
+use Vagebond\Runtype\Actions\ProcessEntities;
+use Vagebond\Runtype\Actions\ResolveProcessableEntities;
+
+class Runtype
+{
+    protected RuntypeConfig $config;
+
+    public function __construct(RuntypeConfig $config)
+    {
+        $this->config = $config;
+    }
+
+    public function generate()
+    {
+        $entities = (new ResolveProcessableEntities(new Finder, $this->config))->handle();
+        $instances = (new ProcessEntities($this->config))->handle($entities);
+        $typescriptTypes = (new ConvertEntities($this->config))->handle($instances);
+
+        (new PersistTypescriptTypes($this->config))->handle($typescriptTypes);
+
+        return $typescriptTypes;
+    }
+}
