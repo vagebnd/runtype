d55aff4059f9eeebb50c1087385ac28ad20e1bbf
Gianluca Riggio <luca@maximumawesome.nl>
2023-02-13 23:15:36 +0100

    added service provider

---
+++ src/RuntypeServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Vagebond\Runtype;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Vagebond\Runtype\Actions\ResolveRequest;
+use Vagebond\Runtype\Commands\RuntypeCommand;
+use Vagebond\Runtype\Contracts\ResolvesRequest;
+
+class RuntypeServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('runtype')
+            ->hasConfigFile()
+            ->hasCommand(RuntypeCommand::class);
+    }
+
+    public function packageRegistered(): void
+    {
+        $this->app->bind(
+            RuntypeConfig::class,
+            fn () => RuntypeConfig::make()
+                ->autoDiscoverPaths(config('runtype.auto_discover_paths'))
+                ->processors(config('runtype.processors'))
+                ->converters(config('runtype.converters'))
+                ->modifiers(config('runtype.modifiers'))
+                ->outputFile(config('runtype.output_file'))
+                ->typeReplacements(config('runtype.type_replacements'))
+        );
+
+        $this->app->bind(ResolvesRequest::class, ResolveRequest::class);
+    }
+}
