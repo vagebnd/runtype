be13171682e1bd3141ebd765a8c08d3438ac7451
Gianluca Riggio <luca@maximumawesome.nl>
2023-02-13 23:14:49 +0100

    added value objects

---
+++ src/Values/TypescriptProperty.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Vagebond\Runtype\Values;
+
+use DateTimeInterface;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+
+class TypescriptProperty
+{
+    public function __construct(
+        private string $name,
+        private string $type,
+        private bool $optional = false,
+    ) {
+    }
+
+    public static function fromValue(string $key, mixed $value): self
+    {
+        return new self($key, self::determineType($value));
+    }
+
+    public function getName(): string
+    {
+        return $this->isOptional() ? $this->name.'?' : $this->name;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function isOptional(): bool
+    {
+        return $this->optional;
+    }
+
+    public function setOptional(bool $optional)
+    {
+        $this->optional = $optional;
+    }
+
+    private static function determineType(mixed $value)
+    {
+        return match (true) {
+            is_string($value) => 'string',
+            is_bool($value) => 'boolean',
+            is_int($value) => 'number',
+            is_float($value) => 'number',
+            is_array($value) => self::processArray($value),
+            $value instanceof DateTimeInterface => 'string',
+            $value instanceof ResourceCollection => TypescriptType::determineName($value->collects).'[]',
+            $value instanceof JsonResource => TypescriptType::determineName(get_class($value)),
+            is_object($value) => self::processArray((array) $value),
+            default => 'any',
+        };
+    }
+
+    private static function processArray(array $value)
+    {
+        if (Arr::isAssoc($value)) {
+            $result = collect($value)
+                ->mapWithKeys(fn ($value, $key) => [$key => self::determineType($value)])
+                ->toJson();
+
+            return Str::replace('"', '', $result);
+        }
+
+        $types = collect($value)->map(fn ($value) => self::determineType($value))->unique();
+
+        return $types->join('|').'[]';
+    }
+}
---
+++ src/Values/TypescriptType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Vagebond\Runtype\Values;
+
+use Illuminate\Support\Collection;
+use ReflectionClass;
+
+class TypescriptType
+{
+    /** @var TypescriptProperty[] */
+    private array $properties = [];
+
+    public function __construct(
+        private string $class
+    ) {
+    }
+
+    public function addProperty(TypescriptProperty $property): self
+    {
+        $this->properties[] = $property;
+
+        return $this;
+    }
+
+    public function addProperties(iterable $properties): self
+    {
+        foreach ($properties as $property) {
+            $this->addProperty($property);
+        }
+
+        return $this;
+    }
+
+    public function listProperties(): Collection
+    {
+        return collect($this->properties);
+    }
+
+    public function getName(): string
+    {
+        return self::determineName($this->class);
+    }
+
+    public function getClass(): string
+    {
+        return $this->class;
+    }
+
+    public function merge(TypescriptType $type): self
+    {
+        $newProperties = $type->listProperties()
+            ->filter(fn ($prop) => ! $this->listProperties()->contains($prop));
+
+        $this->addProperties($newProperties);
+
+        $optionalProperties = $this->listProperties()
+            ->filter(fn ($prop) => ! $type->listProperties()->contains($prop));
+
+        $optionalProperties->each(fn ($prop) => $prop->setOptional(true));
+
+        return $this;
+    }
+
+    public static function determineName(string $class): string
+    {
+        $class = new ReflectionClass($class);
+
+        return $class->getShortName().'Type';
+    }
+}
