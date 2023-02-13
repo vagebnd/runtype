28bfe92a0243897795a9a4332b1537f9b00c7ae6
Gianluca Riggio <luca@maximumawesome.nl>
2023-02-13 23:17:26 +0100

    added testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,36 +1,77 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Vagebond\Runtype\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 use Orchestra\Testbench\TestCase as Orchestra;
-use VendorName\Skeleton\SkeletonServiceProvider;
+use Vagebond\Runtype\RuntypeServiceProvider;
+use Vagebond\Runtype\Tests\Fakes\Models\Category;
+use Vagebond\Runtype\Tests\Fakes\Models\Feature;
+use Vagebond\Runtype\Tests\Fakes\Models\Product;
 
 class TestCase extends Orchestra
 {
     protected function setUp(): void
     {
         parent::setUp();
+        $this->buildWorld();
+    }
 
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
+    private function buildWorld()
+    {
+        Product::factory()
+            ->for(Category::factory())
+            ->has(Feature::factory())
+            ->create();
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            SkeletonServiceProvider::class,
+            RuntypeServiceProvider::class,
         ];
     }
 
-    public function getEnvironmentSetUp($app)
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function defineEnvironment($app)
     {
         config()->set('database.default', 'testing');
+        config()->set('runtype.auto_discover_paths', [__DIR__.'/Fakes/Resources']);
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
-        $migration->up();
-        */
+        $this->migrateDatabase();
+    }
+
+    /**
+     * Define database migrations.
+     *
+     * @return void
+     */
+    public function migrateDatabase()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('features', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id');
+            $table->string('name');
+            $table->timestamps();
+        });
     }
 }
