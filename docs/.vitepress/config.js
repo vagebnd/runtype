eb6187ce6d6c38e63d5754fd4edb2fd231e041d2
Gianluca Riggio <luca@maximumawesome.nl>
2023-03-06 17:29:25 +0100

    added sitemap generator

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,3 +1,10 @@
+import { createWriteStream } from 'node:fs'
+import { resolve } from 'node:path'
+import { SitemapStream } from 'sitemap'
+import { defineConfig } from 'vitepress'
+
+const links = []
+
 export default {
   title: 'RunType',
   description: 'RunType documentation',
@@ -20,5 +27,22 @@ export default {
     ],
 
     socialLinks: [{ icon: 'github', link: 'https://github.com/vagebnd/runtype' }],
+  },
+
+  transformHtml: (_, id, { pageData }) => {
+    if (!/[\\/]404\.html$/.test(id))
+      links.push({
+        // you might need to change this if not using clean urls mode
+        url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
+        lastmod: pageData.lastUpdated,
+      })
+  },
+
+  buildEnd: ({ outDir }) => {
+    const sitemap = new SitemapStream({ hostname: 'https://runtype.vagebond.nl' })
+    const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
+    sitemap.pipe(writeStream)
+    links.forEach((link) => sitemap.write(link))
+    sitemap.end()
   },
 }
